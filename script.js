a9b0102bb8c8ae7979479fdd4b55d99d89f6cd9b
manuelOW <128398955+manuelOW@users.noreply.github.com>
2023-06-12 10:44:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -94,3 +94,90 @@ function verificarEstilo(estiloBoton) {
     document.getElementById('resultado').classList.remove('fireworks-animation');
   }
 }
+function iniciarAnimacionFuegosArtificiales() {
+  particlesJS('resultado', {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: '#ffffff'
+      },
+      shape: {
+        type: 'image',
+        image: {
+          src: 'fireworks.png', // Reemplaza 'fireworks.png' con la ruta de tu imagen de fuegos artificiales
+          width: 100,
+          height: 100
+        }
+      },
+      size: {
+        value: 10,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      move: {
+        enable: true,
+        speed: 3,
+        direction: 'none',
+        random: false,
+        straight: false,
+        out_mode: 'out',
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    interactivity: {
+      detect_on: 'canvas',
+      events: {
+        onhover: {
+          enable: false,
+          mode: 'repulse'
+        },
+        onclick: {
+          enable: false,
+          mode: 'push'
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 200
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
